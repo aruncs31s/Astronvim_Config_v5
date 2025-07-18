2d47e855697d0f21acc73d990155cad21b3d8a94
Arun CS <aruncs31ss@gmail.com>
2025-07-18 20:11:34 +0530

    update

--- lua/community.lua
+++ lua/community.lua
@@ -1,5 +1,3 @@
--- if true then return {} end -- WARN: REMOVE THIS LINE TO ACTIVATE THIS FILE
-
 -- AstroCommunity: import any community modules here
 -- We import this file in `lazy_setup.lua` before the `plugins/` folder.
 -- This guarantees that the specs are processed before any user plugins.
@@ -8,8 +6,17 @@
 return {
   "AstroNvim/astrocommunity",
   { import = "astrocommunity.pack.lua" },
-  { import = "astrocommunity.completion.copilot-lua-cmp" },
+  -- { import = "astrocommunity.completion.copilot-lua-cmp" },
   { import = "astrocommunity.pack.cpp" },
+  { import = "astrocommunity.pack.astro" },
+  { import = "astrocommunity.pack.typescript" },
+  { import = "astrocommunity.pack.markdown" },
+  { import = "astrocommunity.pack.mdx" },
+  { import = "astrocommunity.pack.json" },
+  { import = "astrocommunity.pack.vue" },
+  { import = "astrocommunity.pack.svelte" },
+  { import = "astrocommunity.pack.nvchad-ui" },
+  { import = "astrocommunity.pack.yaml" },
   { import = "astrocommunity.pack.go" },
   { import = "astrocommunity.pack.python" },
   { import = "astrocommunity.pack.typescript" },
--- lua/plugins/mappings.lua
+++ lua/plugins/mappings.lua
@@ -6,7 +6,7 @@ return {
   -- vim.api.nvim_set_keymap("n", "<A-f>", ":Telescope current_buffer_fuzzy_find<CR>", { noremap = true, silent = true }),
   vim.api.nvim_set_keymap("n", "<A-o>", ":Telescope oldfiles<CR>", { noremap = true, silent = true }),
   vim.api.nvim_set_keymap("n", "<A-d>", ":Telescope find_files<CR>", { noremap = true, silent = true }),
-  vim.api.nvim_set_keymap("n", "<A-z>", ":Telescope zoxide list<CR>", { noremap = true, silent = true }),
+  vim.api.nvim_set_keymap("n", "<A-z>", ":Telescope current_buffer_fuzzy_find<CR>", { noremap = true, silent = true }),
   vim.api.nvim_set_keymap("n", "<S-h>", ":bp<CR>", { noremap = true, silent = true }),
   vim.api.nvim_set_keymap("n", "<S-l>", ":bn<CR>", { noremap = true, silent = true }),
   vim.api.nvim_set_keymap("n", "<A-h>", ":bp<CR>", { noremap = true, silent = true }),
